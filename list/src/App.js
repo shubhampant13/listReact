17ada9fc4740f659c3768e4411fe93da56ecf723
Shubham Pant <Shubham.Pant@condecosoftware.com>
2022-08-18 23:56:27 +0530

    Pushing

--- list/src/App.js
+++ list/src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  let [product,setProduct]=useState([
+    {
+        name:"Item1",
+        id: 123456
+    },
+    {
+        name:"Item2",
+        id: 234567
+    }
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>List is below</h1>
+       <table>
+       <tr>
+          <th><h1>Product Name</h1></th>
+          <th><h1>Product ID</h1></th>
+       </tr> 
+       {
+          product.map(function(pro,index){
+              return(
+                  <tr>
+                      <div>
+                          <td><h1>{pro.name}</h1></td>
+                          <td><h1>{pro.id}</h1></td>  
+                      </div>
+                  </tr>
+              )
+          })
+      }
+      </table>
+
     </div>
   );
 }
 
 export default App;
+
+
+
+
